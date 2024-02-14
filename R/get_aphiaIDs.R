3900e2c8183cfcb42ae9850162e52b0b2e36ba09
jackVanish <jackVanish@users.noreply.github.com>
2024-02-14 14:22:04 +0000

    Style code (GHA)

--- R/get_aphiaIDs.R
+++ R/get_aphiaIDs.R
@@ -6,11 +6,11 @@
 ##' we need to.
 ##'
 ##' @param scinames A vector (dataframe column) containing the list of scientific names from a detection extract dataframe in
-##' Surimi. 
+##' Surimi.
 ##'
 ##' @importFrom worrms wm_name2id
 ##'
-##' @return Returns a named list with the scientific name as the key and the aphiaID as the value. 
+##' @return Returns a named list with the scientific name as the key and the aphiaID as the value.
 ##'
 
 # get a table of unique scientific names and aphiaIDs.
@@ -34,13 +34,13 @@ get_unique_aphiaids <- function(scinames) {
 
 ##' @title Consult a lookup table for the aphiaID.
 ##'
-##' @description This is the helper function that we use in the sapply when mutating the WORMS_species_aphia_id into existence. 
+##' @description This is the helper function that we use in the sapply when mutating the WORMS_species_aphia_id into existence.
 ##'
 ##' @param sciname A Scientific name as a string.
-##' @param lookup The named list containing key-value pairs of scientific names and aphiaIDs. 
+##' @param lookup The named list containing key-value pairs of scientific names and aphiaIDs.
 ##'
-##' @return Returns the appropriate aphiaID corresponding to the sciname. 
-##' 
+##' @return Returns the appropriate aphiaID corresponding to the sciname.
+##'
 
 get_aphiaid_from_lookup <- function(sciname, lookup) {
   # Get the aphiaID from the lookup table.
