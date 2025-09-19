56a634a9b269a146bb403fbf4a2a7d757af4b37d
jackVanish <jackVanish@users.noreply.github.com>
2025-09-19 14:57:58 +0000

    Style code (GHA)

--- R/ato_dep_from_otn.R
+++ R/ato_dep_from_otn.R
@@ -1,5 +1,4 @@
-ato_dep_from_otn <- function(otn_file, type="meta") {
-  
+ato_dep_from_otn <- function(otn_file, type = "meta") {
   if (!is.data.frame(otn_file)) {
     extension <- tools::file_ext(otn_detections)
     if (extension == "parquet") {
@@ -8,16 +7,12 @@ ato_dep_from_otn <- function(otn_file, type="meta") {
       otn_file <- read.csv(otn_detections, na = c("", "null", "NA"))
     }
   }
-  
-  if(type == "meta") {
-    
-  }
-  
-  else if(type == "extract") {
-    
-  }
-  
-  else {
+
+  if (type == "meta") {
+
+  } else if (type == "extract") {
+
+  } else {
     message("Invalid type specified. Use either 'meta' (if loading from a metadata file) or 'extract' (if deriving deployment metadata from a detection extract).")
   }
 }
