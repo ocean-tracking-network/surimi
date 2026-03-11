5f1f48001ccedcad3e11dcee5c6240941b9c34e4
Bruce Delo <bruce.delo@gmail.com>
2026-03-11 11:15:13 -0300

    changed all the values of OTN to those of GLATOS.

--- R/glatos_to_ato.R
+++ R/glatos_to_ato.R
@@ -15,34 +15,34 @@
 ##' @export
 ##'
 
-otn_to_ato <- function(otn_detections, otn_receivers = "", otn_tags = "") {
+glatos_to_ato <- function(glatos_detections, glatos_receivers = "", glatos_tags = "") {
   # Determine whether or not we're dealing with a file or a dataframe.
-  if (!is.data.frame(otn_detections)) {
+  if (!is.data.frame(glatos_detections)) {
     # If it's not a dataframe, get the extension.
-    extension <- tools::file_ext(otn_detections)
+    extension <- tools::file_ext(glatos_detections)
     if (extension == "parquet") {
       # If it's a parquet file, read it in with read_parquet.
-      otn_detections <- read_parquet(otn_detections)
+      glatos_detections <- read_parquet(glatos_detections)
     } else {
       # Otherwise, read it in with read.csv.
-      otn_detections <- read.csv(otn_detections, na = c("", "null", "NA"))
+      glatos_detections <- read.csv(glatos_detections, na = c("", "null", "NA"))
     }
   }
   
   # Now we have a dataframe we can start loading into an ATO object. Let's make an instance of the object.
-  OTN_ATO <- new("ATO")
+  GLATOS_ATO <- new("ATO")
   
   # Make the "detections" object,
   det <- make_det(
-    datetime = as.POSIXct(otn_detections$dateCollectedUTC),
+    datetime = as.POSIXct(glatos_detections$detection_timestamp_utc),
     frac_second = NA_real_,
-    receiver_serial = as.integer(otn_detections$receiver),
-    transmitter = otn_detections$tagName,
-    sensor_value = as.numeric(otn_detections$sensorValue),
+    receiver_serial = as.integer(glatos_detections$receiver_sn),
+    transmitter = NA_character_ #Might have to synthesize this from other fields
+    sensor_value = as.numeric(glatos_detections$sensor_value),
     tz = "UTC"
   )
   
-  OTN_ATO <- add(OTN_ATO, det)
+  GLATOS_ATO <- add(GLATOS_ATO, det)
   
   # I used to have a 'derive' argument as in some of the original OTN-to-IMOS functions but then I realised it was safer to just
   # automatically try to derive receiver/tag metadata from the extract if no file is supplied.
@@ -52,21 +52,21 @@ otn_to_ato <- function(otn_detections, otn_receivers = "", otn_tags = "") {
   # In both cases, if a file is supplied, we'll make the metadata objects using the information therein;
   # otherwise, we'll attempt to make approximately correct receiver/tag metadata from only what's contained in
   # the extract.
-  if (otn_receivers != "") {
-    dep <- ato_dep_from_otn(otn_receivers, type = "meta")
+  if (glatos_receivers != "") {
+    dep <- ato_dep_from_glatos(glatos_receivers, type = "meta")
   } else {
-    dep <- ato_dep_from_otn(otn_detections, type = "extract")
+    dep <- ato_dep_from_glatos(glatos_detections, type = "extract")
   }
   
-  OTN_ATO <- add(OTN_ATO, dep)
+  GLATOS_ATO <- add(GLATOS_ATO, dep)
   
-  if (otn_tags != "") {
-    tag <- ato_tag_from_otn(otn_tags, type = "meta")
+  if (glatos_tags != "") {
+    tag <- ato_tag_from_glatos(glatos_tags, type = "meta")
   } else {
-    tag <- ato_tag_from_otn(otn_detections, type = "extract")
+    tag <- ato_tag_from_glatos(glatos_detections, type = "extract")
   }
   
-  OTN_ATO <- add(OTN_ATO, tag)
+  GLATOS_ATO <- add(GLATOS_ATO, tag)
   
-  return(OTN_ATO)
+  return(GLATOS_ATO)
 }
